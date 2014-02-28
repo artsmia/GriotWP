b1690fcc1176d0857b602d37c102638afff52a89
Tom Borger <thomas.borger@gmail.com>
2014-02-28 13:16:02 -0600

    Return objects, not arrays; objects keyed to user-defined id, stories to WP ID

--- endpoint.php
+++ endpoint.php
@@ -2,6 +2,39 @@
 
 global $wpdb;
 
+
+// Get data
+$posts = $wpdb->get_results( 
+	"SELECT ID, post_type, post_content 
+		FROM $wpdb->posts 
+		WHERE post_status = 'publish' 
+		AND ( post_type = 'object' OR post_type = 'story' )", 
+	OBJECT 
+);
+
+$manifest = array(
+	'objects' => array(),
+	'stories' => array(),
+);
+
+foreach( $posts as $post ) {
+
+	if( 'object' == $post->post_type ) {
+
+		// Objects are keyed to user-defined object IDs, rather than WP ID
+		$object_json = json_decode( $post->post_content, true );
+		$object_id = $object_json[ 'id' ];
+		$manifest['objects'][ $object_id ] = $object_json;
+
+	}
+
+	if( 'story' == $post->post_type ) {
+		$manifest['stories'][ $post->ID ] = json_decode( $post->post_content, true );
+	}
+
+}
+
+
 // Get vars appended to 'griot' endpoint
 $query_vars = get_query_var( 'griot' );
 $query_arr = explode( '/', $query_vars );
@@ -21,85 +54,43 @@ else if( ! in_array( $query_arr[0], array( 'objects', 'stories' ) ) ) {
 else if( 1 == count( $query_arr ) && in_array( $query_arr[0], array( 'objects', 'stories' ) ) ) {
 
 	// e.g. '/griot/objects/'
-	$request = $query_arr[0];
+	$request = 'group';
+	$request_type = $query_arr[0];
 
 } 
-else if( is_numeric( $query_arr[1] ) ) {
+else if( 2 == count( $query_arr ) && is_numeric( $query_arr[1] ) ) {
 
 	// e.g. '/griot/objects/370/'
-	$request = $query_arr[1];
+	$request = 'single';
+	$request_type = $query_arr[0];
+	$request_id = $query_arr[1];
 
 }
 else {
 
-	// e.g. '/griot/objects/wrong/'
 	$request = 'all';
-
+	
 }
 
 // Return JSON
 switch( $request ) {
 
+	case 'group':
+
+		$output = $manifest[ $request_type ];
+		echo json_encode( $output );
+		break;
+
+	case 'single':
+
+		$output = $manifest[ $request_type ][ $request_id ];
+		echo json_encode( $output );
+		break;
+
 	case 'all':
-
-		$posts = $wpdb->get_results( "SELECT post_type, post_content FROM $wpdb->posts WHERE post_status = 'publish' AND ( post_type = 'object' OR post_type = 'story' )", OBJECT );
-
-		$output = array(
-			'objects' => array(),
-			'stories' => array(),
-		);
-
-		foreach( $posts as $post ) {
-
-			if( 'object' == $post->post_type ) {
-				$output['objects'][] = json_decode( $post->post_content, true );
-			}
-
-			if( 'story' == $post->post_type ) {
-				$output['stories'][] = json_decode( $post->post_content, true );
-			}
-
-		}
-
-		echo json_encode( $output );
-
-		break;
-
-	case 'objects':
-	case 'stories':
-
-		$post_type = $request == 'objects' ? 'object' : 'story';
-
-		$posts = $wpdb->get_col( 
-			$wpdb->prepare(
-				"SELECT post_content FROM $wpdb->posts WHERE post_status = 'publish' AND post_type = %s",
-				$post_type
-			)
-		);
-
-		$output = array();
-
-		foreach( $posts as $post_content ) {
-
-			$output[] = json_decode( $post_content );
-
-		}
-
-		echo json_encode( $output );
-
-		break;
-
 	default:
 
-		$output = $wpdb->get_var( 
-			$wpdb->prepare( 
-				"SELECT post_content FROM $wpdb->posts WHERE post_status = 'publish' AND ID = %d",
-				$request 
-			)
-		);
-
-		echo $output;
-
+		echo json_encode( $manifest );
 		break;
-
+	
 }
